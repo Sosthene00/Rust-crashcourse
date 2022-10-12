d1c977caf4568131617cd8116e45ca5196a28ade
AntoineSirot <antoine.sirot@edu.devinci.fr>
2022-10-12 18:08:26 +0200

    Impl Triangle

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,13 @@ struct Square<T>
     side: T,
 }
 
+struct Triangle
+{
+    side1 : f64,
+    side2 : f64,
+    side3 : f64
+}
+
 impl Square<f64> {
     fn new( t: f64) -> Self {
         Square { side : t}
@@ -24,6 +31,12 @@ impl Square<String> {
         Square { side : (float).to_string(),
     }
 }
+}
+
+impl Triangle {
+    fn new(c1:f64,c2:f64) -> Self {
+        Triangle { side1 : c1, side2 : c2, side3 : (c1*c1 + c2*c2).sqrt() }
+    }
 }
 
 trait Area {
@@ -65,8 +78,8 @@ fn main() {
     println!("square_float area is {}", square_float.area());
     println!("square_string area is {}", square_string.area());
 
-    /*
     let triangle = Triangle::new(14.9, 20.1);
+    /*
     let pyramid_square = Pyramid::<Square<u32>, f64>::new(square, 24.3);
     let pyramid_triangle = Pyramid::<Triangle<f64>, f64>::new(triangle, 24.3);
 
