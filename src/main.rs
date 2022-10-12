3502f02c7e39764e7f62f56a6e806ddded7b1350
AntoineSirot <antoine.sirot@edu.devinci.fr>
2022-10-12 17:48:53 +0200

    Commit with square shape

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,53 @@
-struct Square<T> -> <T> {
 
+struct Square<T> 
+{
+    side: T,
+}
+
+impl Square<f64> {
+    fn new( t: f64) -> Self {
+        Square { side : t}
+    }
+}
+
+impl Square<u32> {
+    fn new( t: u32) -> Self {
+        Square { side : t}
+    }
+
+}
+
+impl Square<String> {
+    fn new( t: &str) -> Self {
+        let float : f64;
+        float = t.parse().unwrap();
+        Square { side : (float).to_string(),
+    }
+}
+}
+
+trait Area {
+    fn area(&self) -> f64;
+}
+
+impl Area for Square<f64> {
+    fn area(&self) -> f64 {
+        self.side * self.side
+    }
+}
+
+impl Area for Square<u32> {
+    fn area(&self) -> f64 {
+        self.side as f64 * self.side as f64
+    }
+}
+
+
+impl Area for Square<String> {
+    fn area(&self) -> f64 {
+        let tamp : f64 = self.side.parse().unwrap();
+        tamp*tamp
+    }
 }
 
 
@@ -8,14 +56,21 @@ fn main() {
     let square_float = Square::<f64>::new(5.4);
     let square_string = Square::<String>::new("6");
 
+    println!("square area is {}", square.side);
+    println!("square_float area is {}", square_float.side);
+    println!("square_string area is {}", square_string.side);
+
+    
     println!("square area is {}", square.area());
     println!("square_float area is {}", square_float.area());
     println!("square_string area is {}", square_string.area());
 
+    /*
     let triangle = Triangle::new(14.9, 20.1);
     let pyramid_square = Pyramid::<Square<u32>, f64>::new(square, 24.3);
     let pyramid_triangle = Pyramid::<Triangle<f64>, f64>::new(triangle, 24.3);
 
     println!("pyramid_square volume is {}", pyramid_square.volume());
     println!("pyramid_triangle volume is {}", pyramid_triangle.volume());
+    */
 }
