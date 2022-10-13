9d15e2889c172f1ad1be95d81662569b8a6abb97
lucas-kaz <lucaskacz1@gmail.com>
2022-10-13 10:09:48 +0200

    Add Impl, Volume Trait and Struct for Pyramid

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
-
 pub trait Surface<T> {
     fn area(&self) -> T;
+}
 
-    //fn area_float(&self) -> f64;
+pub trait Volume<T>{
+    fn volume(&self) -> T;
 }
 
 struct Square<T>{
     x: T
 }
-
 
 impl Surface<f64> for Square<u32>{
     fn area(&self) -> f64{ 
@@ -27,29 +27,7 @@ impl Surface<f64> for Square<String>{
         self.x.parse::<f64>().unwrap()*self.x.parse::<f64>().unwrap()
     }
 }
-/*
-impl Surface for Square<u32>{
-    fn area(&self) -> f64{
-        self.x
-    }
-}
 
-impl Surface for Square<f64>{
-    fn area_float(&self) -> f64{
-        self.x*self.x
-    }
-}
-
-
-struct Triangle<T>{
-    x: T
-}
-
-struct Pyramid<T>{
-    x: T
-}
-
-*/
 impl Square<u32> {
     fn new(t: u32) -> Self {
         Square { x: t }
@@ -63,27 +41,64 @@ impl Square<f64> {
 }
 
 impl Square<String> {
-    fn new(t: String) -> Self {
-        Square { x: t }
+    fn new(t: &str) -> Self {
+        Square { x: t.to_owned()}
     }
 }
+
+struct Triangle<T>{
+    base: T,
+    height: T
+}
+
+impl Surface<f64> for Triangle<f64>{
+    fn area(&self) -> f64{ 
+        (self.base*self.height)/2 as f64
+    }
+}
+
+impl Triangle<f64> {
+    fn new(b: f64, h: f64) -> Self {
+        Triangle { base: b, height: h }
+    }
+}
+
+struct Pyramid<T, U>{
+    base: T,
+    height: U,  
+}
+
+impl Volume<f64> for Pyramid<Square<u32>, f64>{
+    fn volume(&self) -> f64{ 
+        (self.height/3.0)*(self.base.area()) as f64
+    }
+}
+
+impl Pyramid<Square<u32>, f64> {
+    fn new(b: Square<u32>, h: f64) -> Self {
+        Pyramid { base: b, height: h }
+    }
+}
+
 
 fn main() {
     let square = Square::<u32>::new(5);
     let square_float = Square::<f64>::new(5.4);
-    let square_string = Square::<String>::new("6".to_owned());
+    let square_string = Square::<String>::new("6");
 
     println!("square area is {}", square.area());
     println!("square_float area is {}", square_float.area());
     println!("square_string area is {}", square_string.area());
-    /* 
+     
     let triangle = Triangle::new(14.9, 20.1);
+    println!("triangle area is {}", triangle.area());
+    
     let pyramid_square = Pyramid::<Square<u32>, f64>::new(square, 24.3);
-    let pyramid_triangle = Pyramid::<Triangle<f64>, f64>::new(triangle, 24.3);
+    //let pyramid_triangle = Pyramid::<Triangle<f64>, f64>::new(triangle, 24.3);
 
     println!("pyramid_square volume is {}", pyramid_square.volume());
-    println!("pyramid_triangle volume is {}", pyramid_triangle.volume());
-    */
+    //println!("pyramid_triangle volume is {}", pyramid_triangle.volume());
+    
 }
 
 
