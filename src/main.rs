f9d4ec2ad0475b577bf40390d3bee543c081f37d
Antoine Revel <antoine.revel@gmail.com>
2022-10-13 14:41:45 +0200

    add enum type

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
+mod shape;
+use crate::shape::{MesurableArea, MesurableVolume, Shape2D, Shape3D};
+
 fn main() {
-    let square = Square::<u32>::new(5);
-    let square_float = Square::<f64>::new(5.4);
-    let square_string = Square::<String>::new("6");
+    let square = Shape2D::Square(5.0);
+    let triangle =Shape2D::Triangle(5.0,4.0);
+    let circle =Shape2D::Circle(6.0);
 
     println!("square area is {}", square.area());
+    println!("triangle area is {}", triangle.area());
+
+    let pyramid_square = Shape3D::Pyramid(square,7.0);
+    let pyramid_triangle= Shape3D::Pyramid(triangle,6.0);
+    let cone=Shape3D::Pyramid(circle,7.0);
+
+    println!("pyramid_square volume is {}", pyramid_square.volume());
+    println!("pyramid_triangle volume is {}", pyramid_triangle.volume());
+    println!("cone volume is {}", cone.volume());
+
+
+    /*let square = Square::<u32>::new(5);
+    let square_float = Square::<f64>::new(5.4);
+    let square_string = Square::<&str>::new("6");
+
+
     println!("square_float area is {}", square_float.area());
     println!("square_string area is {}", square_string.area());
 
@@ -12,5 +31,6 @@ fn main() {
     let pyramid_triangle = Pyramid::<Triangle<f64>, f64>::new(triangle, 24.3);
 
     println!("pyramid_square volume is {}", pyramid_square.volume());
-    println!("pyramid_triangle volume is {}", pyramid_triangle.volume());
+    println!("pyramid_triangle volume is {}", pyramid_triangle.volume());*/
 }
+
---
+++ src/shape.rs
@@ -0,0 +1,42 @@
+pub enum Shape2D {
+    Square(f64),
+    Triangle(f64, f64),
+    Circle(f64)
+}
+
+pub trait MesurableArea {
+    fn area(&self) -> f64;
+}
+
+impl MesurableArea for Shape2D {
+    fn area(&self) -> f64 {
+        match &self {
+            Shape2D::Square(x) => { x * x }
+            Shape2D::Triangle(x, y) => { (x * y) / 2.0 }
+            Shape2D::Circle(radius) => {radius.powf(2.0)*std::f64::consts::PI}
+        }
+    }
+}
+
+pub enum Shape3D {
+    Pyramid(Shape2D, f64)
+}
+
+pub trait MesurableVolume {
+    fn volume(&self) -> f64;
+}
+
+impl MesurableVolume for Shape3D {
+    fn volume(&self) -> f64 {
+        match &self {
+            Shape3D::Pyramid(shape2d, h) => { (shape2d.area() * h) / 3.0 }
+        }
+    }
+}
+
+
+
+
+
+
+
